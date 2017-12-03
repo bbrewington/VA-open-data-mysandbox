72389189c151a5f4f7afb7c6b5ca449882d864ff
Brent Brewington <brent.brewington@gmail.com>
2017-12-03 11:58:22 -0500

    check url

--- data_inventory_checkURL.R
+++ data_inventory_checkURL.R
@@ -1,22 +1,24 @@
-library(httr); library(purrr)
+library(httr); library(purrr); library(readr); library(dplyr)
 
 check_url <- function(url) http_status(HEAD(url))$message
 safe_check_url <- safely(check_url)
 
-link_status <- vector(mode = "list", length = nrow(new_dataset))
-for(i in 1:nrow(new_dataset)){
+va_data_inventory <- read_csv("va_data_inventory.csv")
+
+link_status <- vector(mode = "list", length = nrow(va_data_inventory))
+for(i in 1:nrow(va_data_inventory)){
   cat(i, "; ")
-  if(is.na(new_dataset$distribution_accessURL[i])){
-    link_status[[i]] <- safe_check_url(new_dataset$distribution_downloadURL[i])
+  if(is.na(va_data_inventory$distribution_accessURL[i])){
+    link_status[[i]] <- safe_check_url(va_data_inventory$distribution_downloadURL[i])
   } else {
-    link_status[[i]] <- safe_check_url(new_dataset$distribution_accessURL[i])
+    link_status[[i]] <- safe_check_url(va_data_inventory$distribution_accessURL[i])
   }
 }
 
 # This next section is incredibly messy but it seems to work.  Converts the above link status check
-# to character and adds a new column to the "new_dataset"
-new_dataset <- bind_cols(
-  new_dataset, 
+# to character and adds a new column to the "va_data_inventory"
+va_data_inventory <- bind_cols(
+  va_data_inventory, 
   data_frame(link_status_20171202_result = 
                unlist(lapply(link_status, function(x) if_else(is.null(x$result), NA_character_, x$result))),
              link_status_20171202_error = unlist(lapply(link_status, function(x){
@@ -29,10 +31,10 @@ new_dataset <- bind_cols(
     str_replace("Error in curl\\:\\:curl_fetch_memory\\(url, handle \\= handle\\)\\: ", ""))
   ) %>%
   mutate(link_status_20171202 = 
-           if_else(is.na(new_dataset$distribution_accessURL) & is.na(new_dataset$distribution_downloadURL),
+           if_else(is.na(va_data_inventory$distribution_accessURL) & is.na(va_data_inventory$distribution_downloadURL),
                  NA_character_,
                  if_else(is.na(link_status_20171202_result), link_status_20171202_error,
                          link_status_20171202_result))) %>%
   select(-link_status_20171202_result, -link_status_20171202_error)
 
-write_csv(new_dataset, "va_data_inventory.csv", na = "")
+write_csv(va_data_inventory, "va_data_inventory_links_checked_20171202.csv", na = "")
